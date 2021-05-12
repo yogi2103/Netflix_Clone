fc16d9180fa30c46e2890e20cd25503a2b0136bb
Kabira Singh <yogesh.baghel86@gmail.com>
2021-05-12 14:37:57 +0530

    fixing the logo

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import HomeScreeen from './HomeScreen';
+function App() {
+  return (
+    <div className="app">
+      <HomeScreeen />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDb73lwe6kaNTZERqBznh9aNxa61GsZdSk",
+    authDomain: "netflix-clone-fcc46.firebaseapp.com",
+    projectId: "netflix-clone-fcc46",
+    storageBucket: "netflix-clone-fcc46.appspot.com",
+    messagingSenderId: "229678794382",
+    appId: "1:229678794382:web:8b3fa7bd4dc6e3931d6bdf"
+  };
